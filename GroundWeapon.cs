575b69dd1fb33dd4a31bf65990f7aa794147782e
kamilo116 <kamil.sladowski@gmail.com>
2017-03-19 21:35:09 +0100

    Add files via upload

---
+++ GroundWeapon.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/**
+ * Allow to collect and menage of the weapons.
+ */
+
+public class GroundWeapon : MonoBehaviour {
+	public int weaponNumber;
+	private bool isPlayerInTrigger;
+	public Text grabWeapon;
+    //public ParticleSystem particles;
+
+    // Use this for initialization
+    void Start(){
+		grabWeapon.GetComponent<Text> ();
+	}
+
+
+	void OnTriggerStay(Collider col){
+		if (col.tag.Equals ("Player")) {
+			isPlayerInTrigger = true;
+			if(Input.GetKeyDown(KeyCode.E)){
+				col.SendMessage("addWeapon", weaponNumber);
+				isPlayerInTrigger = false;
+				Destroy(gameObject);
+                //Destroy(particles);
+				grabWeapon.enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.tag.Equals ("Player"))
+		isPlayerInTrigger = false;
+
+	}
+
+	void OnGUI(){
+            if (isPlayerInTrigger)
+                grabWeapon.enabled = true;
+            else
+                grabWeapon.enabled = false; 
+	}
+
+}
