c6694b5911976157dd35fde362fd814a509480bc
kamilo116 <kamil.sladowski@gmail.com>
2017-03-19 21:29:43 +0100

    Update and rename takeAmmo.cs to TakeAmmo.cs

--- TakeAmmo.cs
+++ TakeAmmo.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 
 public class takeAmmo : MonoBehaviour {
-
-
 	public float ammunition = 25.0f;
 	public byte gunType = 1;
 	private GunInventory gunInfo;
@@ -16,17 +14,13 @@ public class takeAmmo : MonoBehaviour {
 	void OnTriggerEnter(Collider col){
 
 		if (col.tag.Equals("Player")) {
-			Debug.Log("Znalazles paczke z amunicja");
 			GunInventory inventory = col.GetComponent<GunInventory>();
-			//Transform gun = col.transform.Find("FirstPersonCharacter/My_Glock");
 
-			if(  (inventory.haveGuns[gunType])){ //(inventory.GetComponent<Shoot>().canGetAmmo())&&
+			if(  (inventory.haveGuns[gunType])){ 
 				Debug.Log("take ammo: can get ammo");
-				//gun.SendMessage("addAmmo", new Vector2(ammunition, gunType));
 				inventory.SendMessage("addAmmo", new Vector2(ammunition, gunType));
 				Destroy(gameObject);
 				}
-	
 		}
 	}
 }
