da5d47a3edbd0ca3385063f1b5965cc1ad2c49e6
U-KAMIL-PC\Kamil <kamil.sladowski@gmail.com>
2017-03-26 15:09:02 +0200

    move

---
+++ source/ScoreMenager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreMenager : MonoBehaviour {
+	public static float score;
+	public Text scoreText;
+
+	void Avake () {
+		score = 0.0f;
+		scoreText = GetComponent<Text>();
+	}
+	
+	void Update () {
+		scoreText.text = "Score: " + score;
+	}
+}
